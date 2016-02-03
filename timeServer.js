8d22737c02a67c8564603c9a4ac3a890599d20ad
Nikolai Svakhin <svakhine@gmail.com>
2016-02-03 04:17:37 +0000

    Code beautification: better variable names, passing whole JS object on API request

--- timeServer.js
+++ timeServer.js
@@ -1,12 +1,10 @@
-var  createTimeServer = function(expressServer) {
- 
+var createTimeServer = function(expressServer) {
+
     expressServer.get('/doSynchronize.json', function(ireq, iResponse) {
-        console.log("LALARequest\n");
+        console.log("doSynchronize Request\n");
 
         var result = {
-            fLastDelay: keeper.serverNTPDelta,
-            fAverageDelay: keeper.averageDelta,
-            fNumberOfSamples: keeper.sampleCount,
+            deltaData: keeper.fDeltaData,
         }
 
         iResponse.send(JSON.stringify(result));
@@ -23,16 +21,19 @@ module.exports = {
 
 function Chronos() {
 
-    this.serverNTPDelta = 0.0;
-    this.averageDelta = 0.0;
-    this.sampleCount = 0;
+    this.fDeltaData = {
+        fLastServerNTPDelta: 0.0,
+        fAverageServerNTPDelta: 0.0,
+        fSampleCount: 0.0
+    };
 
-    this.totalDelta = 0.0;
+
+    this.fTotalDelta = 0.0;
 
     var chronosObject = this;
 
     this.Synchronize = function() {
-   //TIME-server query via ntp: https://github.com/moonpyk/node-ntp-client
+        //TIME-server query via ntp: https://github.com/moonpyk/node-ntp-client
 
         var ntpClient = require('ntp-client');
 
@@ -48,17 +49,20 @@ function Chronos() {
             var serverMilliseconds = serverNow.getMilliseconds();
             var serverNTPDelta = serverMilliseconds - ntpMilliseconds;
 
-            chronosObject.serverNTPDelta = serverNTPDelta;
-            chronosObject.totalDelta += serverNTPDelta;
-            chronosObject.sampleCount++;
-            chronosObject.averageDelta = chronosObject.totalDelta / chronosObject.sampleCount;
+            chronosObject.fDeltaData.fLastServerNTPDelta = serverNTPDelta;
+            chronosObject.fTotalDelta += serverNTPDelta;
+            chronosObject.fDeltaData.fSampleCount++;
+            chronosObject.fDeltaData.fAverageServerNTPDelta = chronosObject.fTotalDelta / chronosObject.fDeltaData.fSampleCount;
 
 
             console.log("Current (ServerTime - NTP Time) : " + serverNTPDelta + " ms");
             //  console.log(date); // Mon Jul 08 2013 21:31:31 GMT+0200 (Paris, Madrid (heure d’été)) 
         });
     };
-    this.TickInterval = setInterval(this.Synchronize, 1000);
+    
+    this.Synchronize();
+     
+    this.TickInterval = setInterval(this.Synchronize, 10000);
 }
 
 
