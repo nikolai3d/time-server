aa67ee1b7e59d5bb4121034b292d950b302a6c23
Nikolai Svakhin <svakhine@gmail.com>
2016-02-13 05:00:08 +0000

    Revert "Added fresh versions of deps to bower/package. Gulp copies ntp stuff to client/bower_components now"
    This reverts commit 62dc8705ab5d5cbb7af41970487495222bb202a5.

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,14 @@ var gulp = require('gulp'),
     gutil = require('gulp-util');
     
     
-var ntpJSsource = ["node_modules/socket-ntp/client/ntp.js"]
+    gulp.task('coffee', function() {
 
-var externalJSDependencies = "client/bower_components/";
-    
-gulp.task('copy-ntpjs', function() {
-//Since socket-ntp does not have a bower package, got to copy it from node-modules.
-    gutil.log("COPY-NTP: source path: " + ntpJSsource[0]);
-    var srcNode = gulp.src(ntpJSsource);
-    var destNode = gulp.dest(externalJSDependencies + "/socket-ntp/");
+    var srcNode = gulp.src(coffeeSources);
+    var processNode = gcoffee({
+        bare: true
+    }).on('error', gutil.log);
+    var destNode = gulp.dest('components/scripts');
 
-    srcNode.pipe(destNode);
+    srcNode.pipe(processNode).pipe(destNode);
 
 });
-
-gulp.task('default', ['copy-ntpjs']);//, 'js', 'compass', 'html', 'json', 'images', 'connect', 'watch']);
