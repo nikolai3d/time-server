4bd780d284913b1a16081d5cffc228bf7e9ac55a
Nikolai Svakhin <svakhine@gmail.com>
2016-02-14 21:50:51 +0000

    Added unit test to test for Angular availability

--- client/app/unit_tests/jasmine_unit_tests.js
+++ client/app/unit_tests/jasmine_unit_tests.js
@@ -1,9 +1,24 @@
 /* global expect */
-describe('calculator', function() { //describe specifies a "spec" : logical grouping of tests
+/* global angular */
+
+describe('Synchronizer', function() { //describe specifies a "spec" : logical grouping of tests
+    it('Angular Available', function(){
+        var angularCheck = (typeof(angular) != "undefined");
+        expect(angularCheck).toBe(true);
+    });
+
+    //beforeEach(angular.mock.module('calculatorApp'));
 
     it('1 + 1 should equal 2', function() {
         expect(1 + 1).toBe(2);
     }); // it specifies a single test within a spec
 
+//Other matchers (trutherizers)
+
+// expect(true).toBe(true);
+// expect(false).not.toBe(true);
+// expect(1).toEqual(1);
+// expect('foo').toEqual('foo');
+// expect('foo').not.toEqual('bar');
 
 });
