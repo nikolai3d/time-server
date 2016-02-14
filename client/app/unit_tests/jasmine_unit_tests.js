ed923b6de4c6cedc3d8dfe8490797520714dbc5e
Nikolai Svakhin <svakhine@gmail.com>
2016-02-14 21:29:41 +0000

    First trivial unit test wrapped as a separate JS file

---
+++ client/app/unit_tests/jasmine_unit_tests.js
@@ -0,0 +1,9 @@
+/* global expect */
+describe('calculator', function() { //describe specifies a "spec" : logical grouping of tests
+
+    it('1 + 1 should equal 2', function() {
+        expect(1 + 1).toBe(2);
+    }); // it specifies a single test within a spec
+
+
+});
