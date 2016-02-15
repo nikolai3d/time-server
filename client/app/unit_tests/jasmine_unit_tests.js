131ea4d7df6c74ddcbd12599c6dbbb14479ba720
Nikolai Svakhin <svakhine@gmail.com>
2016-02-15 03:38:49 +0000

    Using tutorial code as reference, created first timeSync controller unit test

--- client/app/unit_tests/jasmine_unit_tests.js
+++ client/app/unit_tests/jasmine_unit_tests.js
@@ -3,11 +3,11 @@
 
 
 describe('Angular Availability', function() { //describe specifies a "spec" : logical grouping of tests
-        it('Angular Available', function(){
+    it('Angular Available', function() {
         var angularCheck = (typeof(angular) != "undefined");
         expect(angularCheck).toBe(true);
     });
-        it('Angular Mock Available', function(){
+    it('Angular Mock Available', function() {
         var angularMockCheck = (typeof(angular.mock) != "undefined");
         expect(angularMockCheck).toBe(true);
     });
@@ -15,18 +15,54 @@ describe('Angular Availability', function() { //describe specifies a "spec" : lo
 
 describe('Synchronizer', function() { //describe specifies a "spec" : logical grouping of tests
 
-    //beforeEach(angular.mock.module('calculatorApp'));
+    beforeEach(angular.mock.module('timesync'));
 
-    it('1 + 1 should equal 2', function() {
-        expect(1 + 1).toBe(2);
+    var $controller;
+    var $scope;
+    var $interval;
+    var $http;
+    var SocketNTPSync;
+
+    beforeEach(angular.mock.inject(function(_$controller_, _$rootScope_) {
+        $controller = _$controller_;
+        $scope = _$rootScope_.$new();
+
+    }));
+
+    beforeEach(angular.mock.inject(function(_$interval_) {
+        $interval = _$interval_;
+    }));
+    
+    beforeEach(angular.mock.inject(function(_$http_) {
+        $http = _$http_;
+    }));
+    
+    beforeEach(angular.mock.inject(function(_SocketNTPSync_) {
+        SocketNTPSync = _SocketNTPSync_;
+    }));
+    
+
+    it('Controller is there', function() {
+
+        //var $intervalSpy = jasmine.createSpy('$interval', $interval);
+
+        var tsController = $controller('TimeSyncController', {
+            $http: $http,
+            $interval: $interval,
+            $scope: $scope,
+            SocketNTPSync: SocketNTPSync
+        });
+
+        expect(tsController.fTitle).toEqual('UberTimeSync');
+
     }); // it specifies a single test within a spec
 
-//Other matchers (trutherizers)
+    //Other matchers (trutherizers)
 
-// expect(true).toBe(true);
-// expect(false).not.toBe(true);
-// expect(1).toEqual(1);
-// expect('foo').toEqual('foo');
-// expect('foo').not.toEqual('bar');
+    // expect(true).toBe(true);
+    // expect(false).not.toBe(true);
+    // expect(1).toEqual(1);
+    // expect('foo').toEqual('foo');
+    // expect('foo').not.toEqual('bar');
 
 });
