4f6371032fa106b7b2bf63873f21e18aad26f9c8
Nikolai Svakhin <svakhine@adobe.com>
2016-04-19 20:27:53 -0700

    Updated ntpsync-client branch tag to pull from. Time.Sync.Controller uses its own function instead of importing, for now

--- client/ntpsync-client/ntpsync-client.js
+++ client/ntpsync-client/ntpsync-client.js
@@ -1,8 +1,11 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 /**
- * AngularInstallIntervalFunction() Attaches An Interval to fire every time each iIntervalTimeMS,
+ * _AngularInstallIntervalFunction() Attaches An Interval to fire every time each iIntervalTimeMS,
  * and makes sure it's destroyed when the scope goes down
  * @param {function}    iFunction: A function to run
  * @param {Number}      iIntervalTimeMS: Number of milliseconds between function runs
@@ -11,7 +14,7 @@
  * @param {Service}     iScope: a $scope or $rootScope service, something we can attach to so we can properly uninstall
  * the interval when the containers go down. https://docs.angularjs.org/api/ng/service/$rootScope
  */
-function AngularInstallIntervalFunction(iFunction, iIntervalTimeMS, iIntervalService, iScope) {
+function _AngularInstallIntervalFunction(iFunction, iIntervalTimeMS, iIntervalService, iScope) {
 
     var intervalHandler = iIntervalService(function () {
         iFunction();
@@ -28,6 +31,7 @@ function AngularInstallIntervalFunction(iFunction, iIntervalTimeMS, iIntervalSer
         iScope.stopSync();
     });
 }
+var AngularInstallIntervalFunction = exports.AngularInstallIntervalFunction = _AngularInstallIntervalFunction;
 },{}],2:[function(require,module,exports){
 'use strict';
 
@@ -237,12 +241,13 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 },{}],3:[function(require,module,exports){
 'use strict';
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }(); /* global gApp */
+
+
+var _angularCommon = require('./angular-common');
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-/* global gApp */
-/* global AngularInstallIntervalFunction */
 (function () {
 
     var kServerNTPDeltaRequestFrequencyMS = 15000; // How often we request server for its Server <-> NTP delta
@@ -308,7 +313,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 
                     // Set up an interval and cancel it once rootScope is going down
                     this.doThePing();
-                    AngularInstallIntervalFunction(function () {
+                    (0, _angularCommon.AngularInstallIntervalFunction)(function () {
                         _this2.doThePing();
                     }, kServerNTPDeltaRequestFrequencyMS, $interval, $rootScope);
                 } /* startPinging */
@@ -329,7 +334,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
         return myServer2NTPSync;
     }]);
 })();
-},{}],4:[function(require,module,exports){
+},{"./angular-common":1}],4:[function(require,module,exports){
 'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
